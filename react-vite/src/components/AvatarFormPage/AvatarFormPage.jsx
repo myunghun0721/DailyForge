1d7b5a1696248638663a4314d5ce65480d9f9ce4
myunghun choi <myunghun0721@gmail.com>
2024-04-24 16:34:12 -0700

    avatar CRUD done

--- react-vite/src/components/AvatarFormPage/AvatarFormPage.jsx
+++ react-vite/src/components/AvatarFormPage/AvatarFormPage.jsx
@@ -159,6 +159,97 @@ function AvatarFormPage() {
               />
             </button>
 
+            <div className="create-avatar">
+              <h1>Update your avatar</h1>
+              <form className="form-create-avatar" onSubmit={handleSubmitUpdate}>
+                <h3>Select body:</h3>
+                <div className="body-grid-container">
+                  {bodies.map((body, id) => (
+                    <div className="body-grid-item" key={id}>
+                      <input
+                        type="radio" name="body"
+                        id={body} className="input-hidden" onChange={() => setBody(body)} />
+                      <label htmlFor={body}>
+                        <img
+                          src={body}
+                          alt="body options" />
+                      </label>
+                    </div>
+                  ))}
+                </div>
+                {error.body ? <h5>{error.body}</h5> : <h5></h5>}
+
+                <h3>Select skin:</h3>
+                <div className="body-grid-container">
+                  {skins.map((skin, id) => (
+                    <div className="body-grid-item" key={id}>
+                      <input
+                        type="radio" name="skin"
+                        id={skin} className="input-hidden" onChange={() => setSkin(skin)} />
+                      <label htmlFor={skin}>
+                        <img
+                          src={skin}
+                          alt="skin options" />
+                      </label>
+                    </div>
+                  ))}
+                </div>
+                {error.skin ? <h5>{error.skin}</h5> : <h5> </h5>}
+
+                <h3>Select hair:</h3>
+                <div className="body-grid-container">
+                  {hairs.map((hair, id) => (
+                    <div className="body-grid-item" key={id}>
+                      <input
+                        type="radio" name="hair"
+                        id={hair} className="input-hidden" onChange={() => sethair(hair)} />
+                      <label htmlFor={hair}>
+                        <img
+                          src={hair}
+                          alt="hair options" />
+                      </label>
+                    </div>
+                  ))}
+                </div>
+                {error.hair ? <h5>{error.hair}</h5> : <h5></h5>}
+
+                <h3>Select extra:</h3>
+                <div className="body-grid-container">
+                  {extras.map((extra, id) => (
+                    <div className="body-grid-item" key={id}>
+                      <input
+                        type="radio" name="extra"
+                        id={extra} className="input-hidden" onChange={() => setExtra(extra)} />
+                      <label htmlFor={extra}>
+                        <img
+                          src={extra}
+                          alt="extra options" />
+                      </label>
+                    </div>
+                  ))}
+                </div>
+                {error.extra ? <h5>{error.extra}</h5> : <h5></h5>}
+
+                <h3>Select background:</h3>
+                <div className="body-grid-container">
+                  {backgrounds.map((background, id) => (
+                    <div className="body-grid-item" key={id}>
+                      <input
+                        type="radio" name="background"
+                        id={background} className="input-hidden" onChange={() => setbackGround(background)} />
+                      <label htmlFor={background}>
+                        <img
+                          src={background}
+                          alt="background options" />
+                      </label>
+                    </div>
+                  ))}
+                </div>
+                {error.background ? <h5>{error.background}</h5> : <h5></h5>}
+
+                <button type="submit" disabled={Object.values(error).length > 0}>Update Avatar</button>
+              </form>
+            </div>
           </div>
         ) :
           <div className="create-avatar">
