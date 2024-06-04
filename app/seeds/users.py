6981fb4cff3d663cca2370937edc4c3e8fc4421a
myunghun choi <myunghun0721@gmail.com>
2024-06-03 17:01:46 -0700

    exp system

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,13 +5,13 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     hun = User(
-        username='Hun', email='hun@aa.io', password='password')
+        username='Hun', exp=100, email='hun@aa.io', password='password')
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', exp=100,email='demo@aa.io', password='password')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', exp=100,email='marnie@aa.io', password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', exp=100,email='bobbie@aa.io', password='password')
 
     db.session.add(hun)
     db.session.add(demo)
