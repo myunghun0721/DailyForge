e55a3391956e3f23c858716bd2961482914a537a
myunghun choi <myunghun0721@gmail.com>
2024-06-03 19:35:50 -0700

    I think I found where issue happening

--- migrations/versions/8c5ecf8edf26_.py
+++ migrations/versions/8c5ecf8edf26_.py
@@ -1,13 +1,16 @@
 """empty message
 
 Revision ID: 8c5ecf8edf26
-Revises: 
+Revises:
 Create Date: 2024-06-03 19:21:04.241926
 
 """
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '8c5ecf8edf26'
@@ -66,6 +69,9 @@ def upgrade():
     sa.ForeignKeyConstraint(['party_id'], ['parties.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
